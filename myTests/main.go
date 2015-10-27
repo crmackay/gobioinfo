3e60ab8702e9f1f6e1994492526e95e5f4da8c87
Chris <crmackay@gmail.com>
2015-10-27 11:25:31 -0400

    getting the build to work

--- myTests/main.go
+++ myTests/main.go
@@ -1,20 +1,20 @@
-package main
-
-import (
-	"fmt"
-	bio "github.com/crmackay/gobioinfo"
-)
-
-func main() {
-	reader := bio.NewFASTQScanner("/Users/christophermackay/Desktop/deepseq_data/pir1/hits-clip/working_data/sample_data/fastq/sblade-test/sample_25000_2.fastq")
-
-	read, _ := reader.NextRead()
-
-	fmt.Println(string(read.Sequence))
-
-	for i := 0; i < 10; i++ {
-		read, _ := reader.NextRead()
-
-		fmt.Println(len(string(read.Sequence)))
-	}
-}
+// package main
+//
+// import (
+// 	"fmt"
+// 	bio "github.com/crmackay/gobioinfo"
+// )
+//
+// func main() {
+// 	reader := bio.NewFASTQScanner("/Users/christophermackay/Desktop/deepseq_data/pir1/hits-clip/working_data/sample_data/fastq/sblade-test/sample_25000_2.fastq")
+//
+// 	read, _ := reader.NextRead()
+//
+// 	fmt.Println(string(read.Sequence))
+//
+// 	for i := 0; i < 10; i++ {
+// 		read, _ := reader.NextRead()
+//
+// 		fmt.Println(len(string(read.Sequence)))
+// 	}
+// }
